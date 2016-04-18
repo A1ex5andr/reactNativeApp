462c4dd32a36e4cab7d31a6cc22e724ff0a2a9cd
Coderbit <sashayu@me.com>
2016-04-18 22:57:00 +0300

    router implemented

--- index.android.js
+++ index.android.js
@@ -1,48 +1,7 @@
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-class reactNativeApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          REACT NATIVE
-        </Text>
-        <Text style={styles.instructions}>
-          redux - hotReloader - babel
-        </Text>
-        <Text style={styles.instructions}>
-          Template for iOS and Android,{'\n'}
-          development
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'orange'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#fff',
-    marginBottom: 5
-  }
-});
+var React = require('react-native');
+var {AppRegistry, Navigator, StyleSheet,Text,View} = React;
+import reactNativeApp from './src/reactNativeApp';
 
 AppRegistry.registerComponent('reactNativeApp', () => reactNativeApp);
--- index.ios.js
+++ index.ios.js
@@ -1,47 +1,7 @@
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-class reactNativeApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Whoooo
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var React = require('react-native');
+var {AppRegistry, Navigator, StyleSheet,Text,View} = React;
+import reactNativeApp from './src/reactNativeApp';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'orange'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#fff',
-    marginBottom: 5
-  }
-});
 AppRegistry.registerComponent('reactNativeApp', () => reactNativeApp);
